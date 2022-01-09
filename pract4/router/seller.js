9337b22be451cc5fa4d22978180a29d023760c91
idarshanapatel <92297286+idarshanapatel@users.noreply.github.com>
2022-01-10 00:29:33 +0530

    Create seller.js

---
+++ pract4/router/seller.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+router.use(express.json());
+const prod = require("../productdetail")
+router.get("/",(req,res)=> res.send("Product page"));
+
+router.post("/addproduct",(req,res)=> {
+ const productlist = req.body;
+ const prolist = prod;
+ prod.push(productlist)
+ console.log(productlist); 
+ res.json({data:prod});
+});
+
+router.post("/fetch",(req,res)=> {
+  res.json({data:prod});
+});
+
+router.post("/dlt",(req,res)=> {
+  const dltdata = req.body;
+  prod.pop(dltdata);
+  res.json({data:prod});
+});  
+
+router.post("/updateproduct",(req,res)=> {
+  const productlist = req.body;
+  prod[1]=productlist;
+  res.json({data:prod});
+});
+
+module.exports = router;
